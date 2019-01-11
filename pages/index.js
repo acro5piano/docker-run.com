c1a4c859c564e5abfa8f3e944f3f55f5d4910616
gosho-kazuya <ketsume0211@gmail.com>
2019-01-11 18:37:12 +0900

    next

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+export default () => (
+  <div>
+    Hello world
+    <p>scoped!</p>
+    <style jsx>{`
+      p {
+        color: blue;
+      }
+      div {
+        background: red;
+      }
+      @media (max-width: 600px) {
+        div {
+          background: blue;
+        }
+      }
+    `}</style>
+    <style global jsx>{`
+      body {
+        background: black;
+      }
+    `}</style>
+  </div>
+)
