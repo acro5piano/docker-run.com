d4a5cc34a65a1c5ef65cf7d323ef002bea9d51e7
gosho-kazuya <ketsume0211@gmail.com>
2019-01-12 15:50:43 +0900

    viewport reset

--- pages/_document.js
+++ pages/_document.js
@@ -8,7 +8,7 @@ export default class MyDocument extends Document {
         <Head>
           <meta charSet="utf-8" />
           <meta http-equiv="X-UA-Compatible" content="ie=edge" />
-          <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+          {/* <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" /> */}
           <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nanum+Gothic|Roboto+Mono" />
           <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
